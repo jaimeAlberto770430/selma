1765b510aa5a294416b4d3c8fe3c3d606da918b4
jaimeAlberto770430 <52228271+jaimeAlberto770430@users.noreply.github.com>
2019-08-16 11:29:06 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 //$('p').hide();
+let i = 0;
+alerta = () => {
+    menu.classList.toggle("show")
+
+}
+let menu = document.getElementById("menu");
+let btnMenu = document.getElementById("btnMenu");
+btnMenu.onclick = alerta;
